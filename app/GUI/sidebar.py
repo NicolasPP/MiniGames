dddc8ad3bc40b2e7dbe42dea89dbfddd581002d1
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-09-24 01:47:54 +0200

    Finished fullscreen button style

--- app/GUI/sidebar.py
+++ app/GUI/sidebar.py
@@ -1,6 +1,6 @@
 import pygame
 from app.app_config import *
-from games.games_config import *
+import games.games_config as gcfg
 from app.GUI.button import Button, Button_Type
 class Sidebar:
 	def __init__(self, width, height, parent, alpha = 1, bg_color = BG_COLOR):
@@ -41,7 +41,7 @@ class Sidebar:
 
 	def add_sidebar_content(self):
 		quit = Button((PADDING, PADDING), (BUTTON_W - PADDING) // 2, BUTTON_H, BG_COLOR, on_click = quit_game, lable = "Quit", offset = self.topleft, show_lable= False,)
-		full_screen = Button(((PADDING * 2) + (BUTTON_W - PADDING) // 2, PADDING), (BUTTON_W - PADDING) // 2, BUTTON_H, "Green", on_click = fullscreen, lable = "Fullscreen", offset = self.topleft, show_lable= False, button_type = Button_Type.SWITCH)
+		full_screen = Button(((PADDING * 2) + (BUTTON_W - PADDING) // 2, PADDING), (BUTTON_W - PADDING) // 2, BUTTON_H, BG_COLOR, on_click = fullscreen, lable = "Fullscreen", offset = self.topleft, show_lable= False, button_type = Button_Type.SWITCH)
 		back = Button((PADDING, (PADDING * 2) + BUTTON_H), BUTTON_W, BUTTON_H, BUTTON_COLOR, on_click = set_game, lable = "Menu", offset = self.topleft, show_lable= True, font_color = FONT_COLOR)	
 		snake = Button((PADDING , (PADDING * 5) + (BUTTON_H * 2)), BUTTON_W, BUTTON_H, BUTTON_COLOR, on_click = set_game,lable = "Snake", offset = self.topleft, show_lable = True, font_color = FONT_COLOR)
 		tictactoe = Button((PADDING , (PADDING * 6) + (BUTTON_H * 3)), BUTTON_W, BUTTON_H, BUTTON_COLOR, on_click = set_game, lable = "Tictactoe", offset = self.topleft, show_lable= True, font_color = FONT_COLOR)
@@ -49,6 +49,7 @@ class Sidebar:
 		quit.style(style_quit, quit)
 		full_screen.set_active_style(fullscreen_active_style, full_screen)
 		full_screen.set_inctive_style(fullscreen_inactive_style, full_screen)
+		full_screen.update_style()
 		return [snake, tictactoe, wordle, quit, back, full_screen]
 
 
@@ -69,7 +70,37 @@ def style_quit(button):
 	pygame.draw.line(button.surface, "Red", (button.width - 5, 5), (5, button.height - 5), 4)
 
 def fullscreen_active_style(button):
-	button.surface.fill("Blue")
+	rects = [
+		#topleft
+		pygame.Rect((FS_RECT_WIDTH - FS_RECT_HEIGHT, 0), (FS_RECT_HEIGHT, FS_RECT_WIDTH)),
+		pygame.Rect((0, FS_RECT_WIDTH - FS_RECT_HEIGHT), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		#topright
+		pygame.Rect((button.width - FS_RECT_WIDTH, 0), (FS_RECT_HEIGHT, FS_RECT_WIDTH)),
+		pygame.Rect((button.width - FS_RECT_WIDTH, FS_RECT_WIDTH - FS_RECT_HEIGHT), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		#bottomleft
+		pygame.Rect((0, button.height - FS_RECT_WIDTH), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		pygame.Rect((FS_RECT_WIDTH - FS_RECT_HEIGHT, button.height - FS_RECT_WIDTH), (FS_RECT_HEIGHT, FS_RECT_WIDTH)),
+		#bottomright
+		pygame.Rect((button.width - FS_RECT_WIDTH, button.height - FS_RECT_WIDTH), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		pygame.Rect((button.width - FS_RECT_WIDTH, button.height - FS_RECT_WIDTH), (FS_RECT_HEIGHT, FS_RECT_WIDTH)),
+	]
+	for rect in rects: pygame.draw.rect(button.surface, "White", rect)
 
 def fullscreen_inactive_style(button):
-	button.surface.fill(button.color)
+	rects = [
+		# topleft
+		pygame.Rect((0,0), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		pygame.Rect((0,0), (FS_RECT_HEIGHT, FS_RECT_WIDTH)),
+		#topright
+		pygame.Rect((button.width - FS_RECT_WIDTH, 0), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		pygame.Rect((button.width - FS_RECT_HEIGHT, 0), (FS_RECT_HEIGHT , FS_RECT_WIDTH)),
+		#bottomleft
+		pygame.Rect((0, button.height-FS_RECT_WIDTH), (FS_RECT_HEIGHT,FS_RECT_WIDTH)),
+		pygame.Rect((0, button.height-FS_RECT_HEIGHT), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+		#bottomright
+		pygame.Rect((button.width - FS_RECT_HEIGHT, button.height - FS_RECT_WIDTH), (FS_RECT_HEIGHT, FS_RECT_WIDTH)),
+		pygame.Rect((button.width - FS_RECT_WIDTH, button.height - FS_RECT_HEIGHT), (FS_RECT_WIDTH, FS_RECT_HEIGHT)),
+	]
+	for rect in rects: pygame.draw.rect(button.surface, "White", rect)
+
+
