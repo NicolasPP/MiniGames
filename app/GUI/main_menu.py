343f1c8df5a320b830a1a72b1bc97da7d7578256
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-09-24 00:23:31 +0200

    main_menu, snake, tictactoe and wordle all inherite from Game

--- app/GUI/main_menu.py
+++ app/GUI/main_menu.py
@@ -1,13 +1,6 @@
-from app.app_config import * 
-class Main_menu:
+from app.app_config import *
+from games.game import Game
+class Main_menu(Game):
 	def __init__(self, app):
-		self.app = app
-		self.surface = self.get_surface()
-
-	def update_surface_size(self):
-		self.surface = self.get_surface()
-
-	def get_surface(self):
-		s = self.app.get_game_surface()
-		s.fill(MAIN_MENU_COLOR)
-		return s
+		super().__init__(app)
+		self.surface.fill(MAIN_MENU_COLOR)
