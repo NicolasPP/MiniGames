65342d2e2644c6a3b088b0e0d4c27cb7fab66802
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-09-23 22:42:26 +0200

    Changed some color selections

--- app/app_config.py
+++ app/app_config.py
@@ -8,8 +8,9 @@ FULLSCREEN = False
 # GUI
 MAIN_MENU_COLOR = color_palette2[1]
 BG_COLOR = color_palette1[0]
-BACK_BUTTON_COLOR = color_palette2[3]
+BUTTON_COLOR = color_palette2[1]
+FONT_COLOR = color_palette2[3]
 PADDING = 10
 BUTTON_W = 80
 BUTTON_H = 30
-LABLE_FONT_SIZE = 20
+LABLE_FONT_SIZE = 15
