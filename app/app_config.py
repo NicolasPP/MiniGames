5ff970926a8ec96098c7c5ff0f10729e10d98332
Nicolas Parucker <nico@pc-22683.home>
2022-09-23 15:57:19 +0200

    Changed config files to use new collor pallet

--- app/app_config.py
+++ app/app_config.py
@@ -1,9 +1,11 @@
+from color.color_palettes import color_palette2, color_palette1
+
 S_WIDTH = 1220
 S_HEIGHT = 700
 FULLSCREEN = False
 
 
 # GUI
-NO_GAME_COLOR = "Orange"
-BG_COLOR = "white"
+NO_GAME_COLOR = color_palette2[1]
+BG_COLOR = color_palette1[0]
 PADDING = 10
--- games/games_config.py
+++ games/games_config.py
@@ -1,5 +1,6 @@
+from color.color_palettes import color_palette1
 # ------ Colours ------
-SNAKE_BG = "Black"
-TICTACTOE_BG = "Blue"
-WORDLE_BG = "Yellow"
+SNAKE_BG = color_palette1[3]       #(231, 171, 121)
+TICTACTOE_BG = color_palette1[3]    #(119, 67, 96)
+WORDLE_BG = color_palette1[3]   #(178, 80, 104)
 # ---------------------
