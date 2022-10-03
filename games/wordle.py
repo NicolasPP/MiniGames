e4f3b45396e32ba76876d2c949cb4a0e5b864cbf
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-03 13:38:08 +0700

    Finished wordle game, only final touches need. Core game loop completed added win screen, added loose screen, added restart functionality fixed alpha problem, where letter_card_alpha was not updating

--- games/wordle.py
+++ games/wordle.py
@@ -14,6 +14,7 @@ class Word_Bank:
 		word = choice(self.words)
 		while word in self.used_words: word = choice(self.words)
 		self.used_words.append(word)
+		print(word)
 		return word
 
 	def get_used_words(self): return [] # make a txt file where we can store used words
@@ -70,6 +71,8 @@ class Letter:
 		self.bg_color = new_state.value
 		self.set_card_style()
 		if self.state is not LSTATE.BLANK: self.card_bg_surface.set_alpha(NORMAL_ALPHA)
+		if self.wordle_game.result == GAME_RESULT.WON: self.card_bg_surface.set_alpha(PAUSE_ALPHA)
+		if self.wordle_game.result == GAME_RESULT.LOST: self.card_bg_surface.set_alpha(LOOSE_ALPHA)
 		self.render_value()
 	@value.setter
 	def value(self, new_value):
@@ -78,14 +81,15 @@ class Letter:
 	# -----------
  
 
-	# --Deletters--
+	# --Deleters--
 	@value.deleter
 	def value(self): del self._value
 	@state.deleter
 	def state(self): del self._state
-	# -------------
+	# ------------
 	
 	
+	def update(self, dt): pass
 
 	def render(self):
 		self.wordle_game.surface.blit(self.card_bg_surface, self.rect.topleft)
@@ -108,10 +112,12 @@ class Letter:
 	def get_value_render(self):
 		value_lable_render = self.font.render(self.value.upper(), True, LETTER_COLOR)
 		s_width, s_height = self.card_bg_surface.get_size()
-		# value_lable_rect = value_lable_render.get_rect(topleft = (0, 0))
 		value_lable_rect = value_lable_render.get_rect(center = (self.rect.width // 2, self.rect.height // 2))
 		return value_lable_render, value_lable_rect
 
+	def reset(self):
+		self.value = ''
+		self.state = LSTATE.BLANK
 
 class Wordle(Game):
 	def __init__(self, app):
@@ -123,39 +129,88 @@ class Wordle(Game):
 		self.letters = []
 		self.create_board()
 		self.game_word = self.word_bank.get_random_word()
-		self.running = True
-		self.result = GAME_RESULT.UNDEFINED
-		
-		print(self.game_word)
+		self._result = GAME_RESULT.UNDEFINED
+		self.post_message_font = pygame.font.Font(None, 50)
+		self.restart_message_font = pygame.font.Font(None, 30)
+		self.restart_alpha = 255
+		self.restart_alpha_change = -1
+		self.SCREENS = Enum(
+			"SCREENS",
+			[
+				("WON",app.get_game_surface(WON_COLOR, alpha = 70)),
+				("LOST",app.get_game_surface(LOST_COLOR, alpha =70))
+			])
+
+	# --Getters--
+	@property
+	def result(self): return self._result
+	# -----------
+
+	# --Setters--
+	@result.setter
+	def result(self, new_result):
+		self._result = new_result
+		self.invalidate_letters_state()
+	# -----------
+
+	# --Deleters--
+	@result.deleter
+	def result(sefl): del self._result
+	# ------------	
+
+	def update(self, dt):
+		for letter in self.letters: letter.update(dt)
+		if self.result is not GAME_RESULT.UNDEFINED: self.update_restart_alpha(dt)
+
+
+	def update_restart_alpha(self, dt):
+		self.restart_alpha +=  (ALPHA_CHANGE * dt * self.restart_alpha_change)
+
+		if self.restart_alpha <= 0:
+			self.restart_alpha = 0
+			self.restart_alpha_change = 1
+
+		if self.restart_alpha >= 255:
+			self.restart_alpha = 255
+			self.restart_alpha_change = -1
 
 	def update_surface_size(self):
 		self.surface = self.app.get_game_surface(self.bg_color)
 		self.resize_letters()
 
+
 	def render(self):
-		if self.running:
-			for letter in self.letters: letter.render()
-			self.app.screen.surface.blit(self.surface, self.app.get_gs_position())
+		for letter in self.letters: letter.render()
 		if self.result is GAME_RESULT.WON: self.render_win()
-		if self.result is GAME_RESULT.LOST: self.render_lost()
+		elif self.result is GAME_RESULT.LOST: self.render_lost()
+		self.app.screen.surface.blit(self.surface, self.app.get_gs_position())
 
 	def render_win(self):
-		'''
-			rgb(83, 141, 78)
-		surface with lower alpha
-		display " GAME WON "
-		play again button/ prompt
-		'''
-		pass
+		self.surface.blit(self.SCREENS.WON.value, (0,0))
+		self.surface.blit(*self.get_center_message_render(" GAME WON  ", self.post_message_font, LETTER_COLOR, NORMAL_ALPHA))
+		self.display_restart_message()
 	def render_lost(self):
-		'''
-			#rgb(58, 58, 60)
-		surface with lower alpha
-		display " GAME LOST "
-		play again button/ prompt
-		'''
-		pass
+		self.surface.blit(self.SCREENS.LOST.value, (0,0))
+		self.surface.blit(*self.get_center_message_render(" GAME LOST ", self.post_message_font, LETTER_COLOR, NORMAL_ALPHA))
+		self.display_restart_message()
+
+	def display_restart_message(self):
+		self.surface.blit(*self.get_center_message_render(" SPACE TO RESTART", self.restart_message_font, LETTER_COLOR, self.restart_alpha, h_offset = 60))
+
+	def get_center_message_render(self, message, font, color, alpha, w_offset = 0, h_offset = 0):
+		message_lable_render = font.render(message, True, color)
+		s_width, s_height = self.surface.get_size()
+		message_lable_rect = message_lable_render.get_rect(center = ((s_width // 2) + w_offset, (s_height// 2) + h_offset))
+		message_lable_render.set_alpha(alpha)
+		return message_lable_render, message_lable_rect
 	
+	def restart_game(self):
+		for letter in self.letters: letter.reset()
+		self.result = GAME_RESULT.UNDEFINED
+		self.game_word = self.word_bank.get_random_word()
+		self.current_letter_index = 0
+		self.current_word_index = 0
+
 	def create_board(self):
 		self.words = []
 		self.letters = []
@@ -168,6 +223,9 @@ class Wordle(Game):
 				word.append(letter)
 				self.letters.append(letter)
 			self.words.append(word)
+
+	def invalidate_letters_state(self):
+		for letter in self.letters: letter.state = letter.state
 
 	def resize_letters(self):
 		x, y = self.get_first_letter_pos()
@@ -198,43 +256,60 @@ class Wordle(Game):
 		current_letter.value = ''
 
 	def check_current_board_word(self):
+		correct_letters = 0
 		if self.current_letter_index != WORD_SIZE: return
+		
+		update_letter_state = {}
+
 		for game_word_letter, user_letter in zip(self.game_word, self.words[self.current_word_index]):
-			if game_word_letter == user_letter.value: user_letter.state = LSTATE.PRESENT_IN_PLACE
+			if game_word_letter == user_letter.value:
+				user_letter.state = LSTATE.PRESENT_IN_PLACE
+				correct_letters += 1
 			elif user_letter.value in self.game_word: user_letter.state = LSTATE.PRESENT_OUT_OF_PLACE
-			else: user_letter.state = LSTATE.NOT_PRESENT 
+			else: user_letter.state = LSTATE.NOT_PRESENT
+
+		if correct_letters == WORD_SIZE: self.result = GAME_RESULT.WON
+		if self.current_word_index == TRYS - 1\
+			and correct_letters != WORD_SIZE: self.result = GAME_RESULT.LOST
 
 		self.current_word_index += 1
 		self.current_letter_index = 0
 
-
+	def get_board_word(self):
+		result = ''
+		current_word = self.words[self.current_word_index]
+		for letter in current_word: result += letter.value
+		return result
 	def parse_event(self, event):
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_BACKSPACE: self.remove_letter_value() # delete
-			if event.key == pygame.K_RETURN: self.check_current_board_word() # enter
-			if event.key == pygame.K_a: self.add_letter_value('a')
-			if event.key == pygame.K_b: self.add_letter_value('b')
-			if event.key == pygame.K_c: self.add_letter_value('c')
-			if event.key == pygame.K_d: self.add_letter_value('d')
-			if event.key == pygame.K_e: self.add_letter_value('e')
-			if event.key == pygame.K_f: self.add_letter_value('f')
-			if event.key == pygame.K_g: self.add_letter_value('g')
-			if event.key == pygame.K_h: self.add_letter_value('h')
-			if event.key == pygame.K_i: self.add_letter_value('i')
-			if event.key == pygame.K_j: self.add_letter_value('j')
-			if event.key == pygame.K_k: self.add_letter_value('k')
-			if event.key == pygame.K_l: self.add_letter_value('l')
-			if event.key == pygame.K_m: self.add_letter_value('m')
-			if event.key == pygame.K_n: self.add_letter_value('n')
-			if event.key == pygame.K_o: self.add_letter_value('o')
-			if event.key == pygame.K_p: self.add_letter_value('p')
-			if event.key == pygame.K_q: self.add_letter_value('q')
-			if event.key == pygame.K_r: self.add_letter_value('r')
-			if event.key == pygame.K_s: self.add_letter_value('s')
-			if event.key == pygame.K_t: self.add_letter_value('t')
-			if event.key == pygame.K_u: self.add_letter_value('u')
-			if event.key == pygame.K_v: self.add_letter_value('v')
-			if event.key == pygame.K_w: self.add_letter_value('w')
-			if event.key == pygame.K_x: self.add_letter_value('x')
-			if event.key == pygame.K_y: self.add_letter_value('y')
-			if event.key == pygame.K_z: self.add_letter_value('z')
+			if self.result == GAME_RESULT.UNDEFINED:
+				if event.key == pygame.K_BACKSPACE: self.remove_letter_value() # delete
+				if event.key == pygame.K_RETURN: self.check_current_board_word() # enter
+				if event.key == pygame.K_a: self.add_letter_value('a')
+				if event.key == pygame.K_b: self.add_letter_value('b')
+				if event.key == pygame.K_c: self.add_letter_value('c')
+				if event.key == pygame.K_d: self.add_letter_value('d')
+				if event.key == pygame.K_e: self.add_letter_value('e')
+				if event.key == pygame.K_f: self.add_letter_value('f')
+				if event.key == pygame.K_g: self.add_letter_value('g')
+				if event.key == pygame.K_h: self.add_letter_value('h')
+				if event.key == pygame.K_i: self.add_letter_value('i')
+				if event.key == pygame.K_j: self.add_letter_value('j')
+				if event.key == pygame.K_k: self.add_letter_value('k')
+				if event.key == pygame.K_l: self.add_letter_value('l')
+				if event.key == pygame.K_m: self.add_letter_value('m')
+				if event.key == pygame.K_n: self.add_letter_value('n')
+				if event.key == pygame.K_o: self.add_letter_value('o')
+				if event.key == pygame.K_p: self.add_letter_value('p')
+				if event.key == pygame.K_q: self.add_letter_value('q')
+				if event.key == pygame.K_r: self.add_letter_value('r')
+				if event.key == pygame.K_s: self.add_letter_value('s')
+				if event.key == pygame.K_t: self.add_letter_value('t')
+				if event.key == pygame.K_u: self.add_letter_value('u')
+				if event.key == pygame.K_v: self.add_letter_value('v')
+				if event.key == pygame.K_w: self.add_letter_value('w')
+				if event.key == pygame.K_x: self.add_letter_value('x')
+				if event.key == pygame.K_y: self.add_letter_value('y')
+				if event.key == pygame.K_z: self.add_letter_value('z')
+			else:
+				if event.key == pygame.K_SPACE: self.restart_game()
