6347af77420b3de8d9c2acc97457e3f92cdf17c1
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-09 21:20:41 +0700

    Added some todos

--- app/GUI/main_menu.py
+++ app/GUI/main_menu.py
@@ -1,5 +1,28 @@
 from config.app_config import *
 from games.game import Game
+
+'''
+TODO : add user's stats
+	- Snake
+		- max score in snake
+		- amount of games played
+	- Wordle
+		- amount of words guessed right
+		- amount og words guessed wrong
+		- little screenshot / smaller version of board of best game (game with smallest amount of moves)
+	- Tictactoe
+		- player One games won
+		- player Two games won
+	- sudoku
+		- games solved
+		- fastest game solved
+		- animation of fastest game solved
+	- kakuro
+		- games solved
+		- fastest game solved
+		- animation of fastest game solved
+'''
+
 class Main_menu(Game):
 	def __init__(self, app):
 		super().__init__(app, MAIN_MENU_COLOR)
--- app/main.py
+++ app/main.py
@@ -3,11 +3,20 @@ from config.app_config import *
 
 
 '''
+TODO: make system where only the selected game is instanciated
+TODO: make sidebar be able to scroll, take any amount of buttons
 TODO: make class for drawing letter on the screen
 	  right now evrything is too hard coded. pretty sure
 	  could write one function to complete all functionality needed
 TODO: add click library, add option to run without pyenchat.
 	  couldnt find any dictionaries when using pyenchant on windows
+TODO: Games to add in the future
+		- Game of life
+		- algorithm visualizer
+		- sudoku
+		- kakuro
+		- boids flocking
+		- level generation (maybe)
 '''
 
 
