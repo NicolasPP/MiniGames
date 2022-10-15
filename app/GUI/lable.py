14feefd6ec1f754fe549f25ea9abfa1c357cd93e
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-15 21:34:44 +0700

    Added alpha property to lable class

--- app/GUI/lable.py
+++ app/GUI/lable.py
@@ -2,18 +2,20 @@ import pygame
 
 
 class Lable:
-	def __init__(self, pos, message, font_size, color, font = False, r_topleft = False ):
+	def __init__(self, pos, message, font_size, color, alpha, font = False, r_topleft = False ):
 		self.pos = pos
 		self.message = message
 		self.font = font if font else pygame.font.Font(None, font_size)
 		self.color = color
 		self.r_topleft = r_topleft # default is center
+		self.alpha = alpha
 
 
 	def get_lable_blit(self):
 		message_render = self.font.render(self.message, True, self.color)
 		message_rect = message_render.get_rect(center = self.pos)
 		if self.r_topleft: message_rect = message_render.get_rect(topleft = self.pos)
+		message_render.set_alpha(self.alpha)
 		return message_render, message_rect
 
 
