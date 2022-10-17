957c2187e21e0cf44aa059cd465915b6352a6200
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-17 14:47:37 +0700

    Made message_render and message_rect memebers of Lable class made message a @property memeber of Lable class so I can recreate message_render and message_rect when lable message is changed

--- app/GUI/lable.py
+++ app/GUI/lable.py
@@ -4,16 +4,34 @@ import pygame
 class Lable:
 	def __init__(self, pos, message, font_size, color, alpha, font = False, r_topleft = False ):
 		self.pos = pos
-		self.message = message
+		self._message = message
 		self.font = font if font else pygame.font.Font(None, font_size)
 		self.color = color
 		self.r_topleft = r_topleft # default is center
 		self.alpha = alpha
 
+		self.message_render = self.font.render(self.message, True, self.color)
+		self.message_rect = self.message_render.get_rect(center = self.pos)
+
+	@property
+	def message(self): return self._message
+
+	@message.setter
+	def message(self, new_message): 
+		self._message = new_message
+		self.message_render = self.font.render(self.message, True, self.color)
+		self.message_rect = self.message_render.get_rect(center = self.pos)		
+
+	@message.deleter
+	def message(self): del self._message
+
 
 	def get_lable_blit(self):
+		if self.r_topleft: self.message_rect = self.message_render.get_rect(topleft = self.pos)
+		self.message_render.set_alpha(self.alpha)
+		return self.message_render, self.message_rect
+
+	def get_lable_rect(self):
 		message_render = self.font.render(self.message, True, self.color)
-		message_rect = message_render.get_rect(center = self.pos)
-		if self.r_topleft: message_rect = message_render.get_rect(topleft = self.pos)
-		message_render.set_alpha(self.alpha)
-		return message_render, message_rect
+		message_render_rect = message_render.get_rect(topleft = (0, 0))
+		return message_render_rect
