328a73bdac6f5c90a1515c3a9b4e44f0388b8db3
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-24 23:01:59 +0700

    Type hint for utils

--- app/utils/time.py
+++ app/utils/time.py
@@ -1,24 +1,16 @@
 import pygame
 regular_interval = pygame.time.get_ticks()
 
-def regular_interval_tick_wait(time):
-    global regular_interval
-    now = pygame.time.get_ticks()
-    if now - regular_interval >= time:
-        regular_interval = now
-        return True
-    return False
-
-
 class Time_Man:
     def __init__(self):
-        self.waited_time = 0
+        self.waited_time : float = 0
 
-    def dt_wait(self, dt, time):
+    def dt_wait(self, dt : float, time : float) -> bool:
         if self.waited_time >= time / 1000:
             self.waited_time = 0
             return True
         self.waited_time += dt
+        return False
 
 
 
