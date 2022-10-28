239fde99dead00ce4ac043567dffecd907055d0c
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-28 21:16:37 +0700

    moved blinking logic into lable class

--- app/GUI/components/lable.py
+++ app/GUI/components/lable.py
@@ -1,4 +1,5 @@
 import pygame
+from config.games_config import *
 from GUI.components.component import Component
 
 
@@ -16,22 +17,36 @@ class Lable(Component):
 		self.font = pygame.font.Font(None, font_size)
 		self.r_topleft = r_topleft # default is center
 		self.pos = pos
-		self.update_surface()
+		self.blink_alpha = alpha
+		self.blink_speed = ALPHA_CHANGE
+		self.blink_change = -1
 
-		
+		self.center()
+	def blink(self, dt):
+		self.alpha +=  (self.blink_speed * dt * self.blink_change)
 
+		if self.alpha <= 0:
+			self.alpha = 0
+			self.blink_change = 1
+
+		if self.alpha >= 255:
+			self.alpha = 255
+			self.blink_change = -1
 	@property
 	def message(self): return self._message
 
 	@message.setter
 	def message(self, new_message): 
 		self._message = new_message
-		self.update_surface()	
+		self.update_message()	
 
 	@message.deleter
 	def message(self): del self._message
 
-
-	def update_surface(self):
+	def center(self):
 		self.surface = self.font.render(self.message, True, self.color)
-		self.rect = self.surface.get_rect(center = self.pos)
+		self.rect = self.surface.get_rect(center = self.rect.topleft)
+
+	def update_message(self):
+		self.surface = self.font.render(self.message, True, self.color)
+		self.rect = self.surface.get_rect(center = self.rect.center)
