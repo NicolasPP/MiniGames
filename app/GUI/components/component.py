6e61032eee2b2b45de2dfbc6d3c3860d3b839cd0
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-28 21:18:25 +0700

    Added render function where you pass in the destination surface instead of always blitting to parents surface

--- app/GUI/components/component.py
+++ app/GUI/components/component.py
@@ -13,27 +13,26 @@ class Component:
 		self.alpha = alpha
 		self.processed = False
 		self.rect = pygame.Rect(pos, size)
-		self.color = color
+		self.color = (255, 255, 255) if color == (-1,-1,-1) else color
 		self.surface = pygame.Surface(size)
-		self.surface.fill(self.color)
+		self.show = True
 		
 
 	def update_pos(self, pos_change):
 		self.rect.topleft = tuple(pygame.math.Vector2(self.rect.topleft) + pos_change)
 
 
-	def set_size(self, size):
-		pos = self.rect.x , self.rect.y
-		self.rect = pygame.Rect(pos, size)
-		self.surface = pygame.Surface(size)
-		self.surface.fill(self.color)
-
 	def render(self, set_alpha = False):
+		if not self.show: return
 		self.parent.surface.blit(*self.get_surface_blit(set_alpha = set_alpha))
+
+	def render_dest(self, dest, set_alpha = False):
+		if not self.show: return
+		dest.blit(*self.get_surface_blit(set_alpha))
 
 	def get_surface_blit(self, set_alpha = False):
 		if set_alpha: self.surface.set_alpha(self.alpha)
 		return self.surface, self.rect
 
-	def is_clicked(self, mouse_pos):
+	def is_hovered(self, mouse_pos):
 		return self.rect.collidepoint(mouse_pos)
