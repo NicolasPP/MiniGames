f8932e646e096fa829729fd7f901812ac55c2932
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-28 21:19:51 +0700

    Type hint game.py and removed unused function

--- app/games/game.py
+++ app/games/game.py
@@ -6,22 +6,23 @@ if TYPE_CHECKING:
 from config.games_config import *
 import pygame
 from dataclasses import dataclass
-from GUI.containers import Relative_Container
+from GUI.components.containers import Container
+from GUI.components.button import Button
+from GUI.components.lable import Lable
+
 
 class Game:
 	def __init__(self, 
 				app : Minigames, 
-				bg_color : tuple[int, int, int] = GAME_BG
+				color : tuple[int, int, int] = GAME_BG
 				) -> None:
-	
-		self.bg_color = bg_color
+		self.color = color
 		self.app = app
 
-		self._surface : pygame.Surface = self.get_game_surface(self.bg_color)
-		self.rect : pygame.rect.Rect = self.surface.get_rect(topleft = app.get_game_pos())
+		self._surface : pygame.Surface = self.get_game_surface(self.color)
+		self.rect : pygame.rect.Rect = self.surface.get_rect(topleft = (0, 0))
 				
 		self.paused : bool = True
-		# self.root_container = Relative_Container(self, (0,0), app.get_game_size(), alpha, color)
 
 	@property
 	def surface(self) -> pygame.Surface: return self._surface
@@ -29,7 +30,7 @@ class Game:
 	@surface.setter
 	def surface(self, new_surface : pygame.Surface) -> None: 
 		self._surface = new_surface
-		self.rect = new_surface.get_rect(topleft = self.app.get_game_pos())
+		self.rect = new_surface.get_rect(topleft = (0,0))
 
 	@surface.deleter
 	def surface(self) -> None: del self._surface
@@ -39,23 +40,36 @@ class Game:
 	def parse_event(self, event : pygame.event.Event) -> None: pass
 
 	def render(self) -> None: 
-		self.app.screen.surface.blit(self.surface, self.app.get_game_pos())
-	def update_surface_size(self) -> None: self.surface = self.get_game_surface(self.bg_color)
-	def render_message(self, *lable_ids : tuple[str]):
-		for l_id in lable_ids:
-			lable = self.lables[l_id]['lable']
-			lable_surface  = self.lables[l_id]['surface']
-			if lable_surface : self.surface.blit(lable_surface, (0,0))  	
-			lable.render(set_alpha = True)
+		self.app.screen.surface.blit(self.surface, (0,0))
+	def update_surface_size(self) -> None: self.surface = self.get_game_surface(self.color)
 
 	def get_game_surface(self, 
 						color : tuple[int, int, int], 
 						alpha : int = NORMAL_ALPHA
 		) -> pygame.Surface:
-		game_surface = pygame.Surface(self.app.get_game_size())
+		game_surface = pygame.Surface(self.app.screen.get_current_size())
 		game_surface.set_alpha(alpha)
 		game_surface.fill(color)
 		return game_surface
+
+class Game_GUI:
+	def __init__(self, game : Game):
+		self.game = game
+		self.containers : dict[str, Container] = {}
+		self.lables : dict[str, Lable] = {}
+		self.buttons : dict[str, Button] = {}
+		self._surface =  game.surface
+
+	@property
+	def surface(self): return self.wordle_game.surface
+	@surface.setter
+	def surface(self, new_surface): self._surface = new_surface
+	@surface.deleter
+	def surface(self): del self._surfacex
+
+	def populate_GUI(self):
+		assert "not implemented"
+
 
 
 
