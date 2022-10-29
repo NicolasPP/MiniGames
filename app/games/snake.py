b05e3a5eec45a1b58f56b866f245b27225714a46
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-29 17:18:04 +0700

    Type hints for snake.py

--- app/games/snake.py
+++ app/games/snake.py
@@ -6,6 +6,7 @@ import pygame
 from random import choice
 from enum import Enum
 from utils.time import Time_Man
+from typing import Tuple 
 
 
 class DIRECTION(Enum):
@@ -44,60 +45,60 @@ class DIRECTION(Enum):
 class SNAKE:
 	def __init__(self, snake_game):
 		self.snake_game = snake_game
-		self.rect = get_mid_cell(snake_game)
+		self.rect : pygame.rect.Rect = get_mid_cell(snake_game.grid)
 		self.rect.width -= 1
 		self.rect.height -= 1
-		self.size = 0
-		self.body = []
-		self.color = SNAKE_COLOR
-		self.timer = Time_Man()
-		self._direction = choice(list(DIRECTION))
-		self.distance_to_move = 0
-		self._pos = pygame.math.Vector2(self.rect.topleft)
-		self.speed = S_CELL_SIZE / (TIME_TO_COVER_CELL / 1000)
-		self.alive = True
+		self.size : int = 0
+		self.body : list[pygame.rect.Rect] = []
+		self.color : tuple[int, int, int] = SNAKE_COLOR
+		self.timer : Time_Man = Time_Man()
+		self._direction : DIRECTION = choice(list(DIRECTION))
+		self.distance_to_move : float = 0
+		self._pos : pygame.math.Vector2 = pygame.math.Vector2(self.rect.topleft)
+		self.speed : float = S_CELL_SIZE / (TIME_TO_COVER_CELL / 1000)
+		self.alive : bool = True
 
 	# -- Direction --
 	@property
-	def direction(self): return self._direction
+	def direction(self) -> DIRECTION: return self._direction
 
 	@direction.setter
-	def direction(self, new_direction):
+	def direction(self, new_direction : DIRECTION) -> None:
 		assert isinstance(new_direction, DIRECTION)
 		if new_direction is self._direction: return
 		if new_direction.value == self.direction.inverse(): return
 		self._direction = new_direction
 
 	@direction.deleter
-	def direction(self): del self._direction
+	def direction(self) -> None: del self._direction
 	
 	# -------------
 
 
 	# -- Pos --
 	@property
-	def pos(self): return self._pos
+	def pos(self) -> pygame.math.Vector2: return self._pos
 
 
 	@pos.setter
-	def pos(self, new_pos): 
+	def pos(self, new_pos : pygame.math.Vector2) -> None: 
 		self._pos = new_pos
-		x, y = new_pos
+		x, y = new_pos.x, new_pos.y
 		self.rect.x, self.rect.y = round(x), round(y)
 
 	@pos.deleter
-	def pos(self): del self._pos
+	def pos(self) -> None: del self._pos
 	# -------------
 
 
 	# -- Render --
-	def render(self):
+	def render(self) -> None:
 		for bdy in self.body: self.snake_game.surface.blit(get_rect_surface(self.snake_game, bdy, self.color), bdy.topleft)
 		self.snake_game.surface.blit(get_rect_surface(self.snake_game, self.rect, self.color), self.rect.topleft)
 	# ------------
 
 	# -- Update --
-	def update(self, dt):
+	def update(self, dt : float) -> None:
 		self.body_collision()
 		self.wall_collision()
 		self.food_collision()
@@ -107,19 +108,19 @@ class SNAKE:
 	
 
 	# -- Collision Detection --
-	def body_collision(self):
+	def body_collision(self) -> None:
 		p1, p2 = get_collision_points(self)
 		for bdy in self.body:
 			if bdy.collidepoint(p1) or bdy.collidepoint(p2):
 				self.die()
 	
-	def wall_collision(self):
+	def wall_collision(self) -> None:
 		width, height = self.snake_game.surface.get_size()
 		p1, p2 = get_collision_points(self)
 		if p1[0] >= width or p1[0] <= 0 \
 			or p1[1] >= height or p1[1] <= 0: self.die()
 	
-	def food_collision(self):
+	def food_collision(self) -> None:
 		collided = []
 		for food in self.snake_game.foods:
 			if food.colliderect(self.rect):
@@ -132,23 +133,23 @@ class SNAKE:
 
 
 	# -- Game Logic --
-	def die(self): 
+	def die(self) -> None: 
 		self.alive = False
 		self.snake_game.snake_GUI.lables['final_score_lable'].message = f'{self.snake_game.score}'
 	
-	def move(self, dt):
+	def move(self, dt : float) -> None:
 		if self.distance_to_move <= 0: return
 		dist = self.speed * dt
 		if self.distance_to_move - dist < 0: dist += (self.distance_to_move - dist)
 		self.pos = self.pos + (self.direction * dist)
 		self.distance_to_move -= dist	
 	
-	def add(self, rect):
+	def add(self, rect : pygame.rect.Rect) -> None:
 		self.body.append(rect)
 		body_size = len(self.body)
 		if body_size > self.size: self.body = self.body[1:body_size]
 	
-	def set_move_distance(self, dt):
+	def set_move_distance(self, dt : float) -> None:
 		if self.timer.dt_wait(dt, SNAKE_MOVE_FREQ + TIME_TO_COVER_CELL):
 			self.distance_to_move += S_CELL_SIZE
 			prev_rec = self.rect.copy()
@@ -159,25 +160,25 @@ class SNAKE:
 class Snake_GUI:
 	def __init__(self, snake_game):
 		self.snake_game = snake_game
-		self.containers = {}
-		self.lables = {}
-		self._surface = snake_game.surface
+		self.containers : dict[str, Container] = {}
+		self.lables : dict[str, Lable] = {}
+		self._surface : pygame.Surface = snake_game.surface
 		self.populate_GUI()
 
 	@property
-	def surface(self): return self.snake_game.surface
+	def surface(self) -> pygame.Surface: return self.snake_game.surface
 	@surface.setter
-	def surface(self, new_surface): self._surface = new_surface
+	def surface(self, new_surface : pygame.Surface) -> None: self._surface = new_surface
 	@surface.deleter
-	def surface(self): del self._surfacex
+	def surface(self) -> None: del self._surface
 	
-	def populate_GUI(self):
+	def populate_GUI(self) -> None:
 		self.create_containers()
 		self.create_lables()
 		self.populate_containers()
 		
 
-	def create_containers(self):
+	def create_containers(self) -> None:
 		game_container = Relative_Container(self.snake_game, pygame.display.get_surface().get_size(), root = True)
 		paused_container = Relative_Container(self.snake_game, pygame.display.get_surface().get_size(), root = True, alpha = PAUSE_ALPHA)
 		loose_container = Relative_Container(self.snake_game, pygame.display.get_surface().get_size(), root = True, alpha = LOOSE_ALPHA)
@@ -196,7 +197,7 @@ class Snake_GUI:
 		self.containers['loose_container'] = loose_container
 		
 
-	def populate_containers(self):
+	def populate_containers(self) -> None:
 		s_width, s_height = pygame.display.get_surface().get_size()
 		center = s_width // 2, s_height // 2
 		score_pos = s_width - 30, 30
@@ -217,7 +218,7 @@ class Snake_GUI:
 		loose_container.add_component(final_score_lable, center)
 
 
-	def create_lables(self):
+	def create_lables(self) -> None:
 		score_lable = Lable(self.snake_game, f'{self.snake_game.score}', SCORE_FONT_SIZE, LETTER_COLOR, PAUSE_ALPHA)
 		paused_lable = Lable(self.snake_game, " PAUSED ", PAUSE_FONT_SIZE , SCORE_COLOR ,NORMAL_ALPHA)
 		retry_lable = Lable(self .snake_game, " SPACE TO RETRY ", 30 , SCORE_COLOR, NORMAL_ALPHA)
@@ -231,21 +232,21 @@ class Snake_GUI:
 
 
 class Snake(Game):
-	def __init__(self, app):
+	def __init__(self, app) -> None:
 		super().__init__(app)
-		self.grid = []
-		self.cells = []
-		self.foods = []
+		self.grid :list[list[pygame.rect.Rect]] = []
+		self.cells :list[pygame.rect.Rect] = []
+		self.foods :list[pygame.rect.Rect] = []
 		self.score = 0
 
-		self.snake_GUI = Snake_GUI(self)
-		self.food_timer = Time_Man()
+		self.snake_GUI : Snake_GUI = Snake_GUI(self)
+		self.food_timer : Time_Man = Time_Man()
 		
 		create_grid(self)
-		self.snake = SNAKE(self)
+		self.snake : SNAKE = SNAKE(self)
 		
 	# -- Render --
-	def render(self):
+	def render(self) -> None:
 		if self.snake.alive:
 			for food in self.foods: self.surface.blit(get_rect_surface(self, food, FOOD_COLOR), food.topleft)
 			self.snake.render()
@@ -261,7 +262,7 @@ class Snake(Game):
 
 
 	# -- Update --
-	def update(self, dt):
+	def update(self, dt : float) -> None:
 		if self.paused:
 			self.snake_GUI.lables['paused_lable'].blink(dt) 
 		else:
@@ -271,7 +272,7 @@ class Snake(Game):
 			else: self.snake_GUI.lables['retry_lable'].blink(dt) 
 
 	
-	def update_surface_size(self):
+	def update_surface_size(self) -> None:
 		self.surface = self.get_game_surface(self.color)
 		self.snake_GUI = Snake_GUI(self)
 		create_grid(self)
@@ -279,7 +280,7 @@ class Snake(Game):
 
 
 	# -- Player Input --
-	def parse_event(self, event):
+	def parse_event(self, event : pygame.event.Event) -> None:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE: toggle_pause(self)
 			if event.key == pygame.K_UP or event.key == pygame.K_w: self.snake.direction = DIRECTION.UP
@@ -291,15 +292,15 @@ class Snake(Game):
 
 
 # -- SNAKE helpers --
-def get_collision_points(snake):
+def get_collision_points(snake : SNAKE) -> tuple[list[float], list[float]]:
 		h_offset = S_CELL_SIZE // 2
 		direc_x, direc_y = snake.direction.value
 		center_x, center_y = snake.rect.center
 		center_head_x = center_x + (direc_x * h_offset)
 		center_head_y = center_y + (direc_y * h_offset)
 
-		p1 = [center_head_x, center_head_y]
-		p2 = [center_head_x, center_head_y]
+		p1 = [float(center_head_x), float(center_head_y)]
+		p2 = [float(center_head_x), float(center_head_y)]
 
 		if direc_x == 0: 
 			p1[0] += h_offset
@@ -308,28 +309,28 @@ def get_collision_points(snake):
 			p1[1] += h_offset
 			p2[1] -= h_offset
 
-		return tuple(p1), tuple(p2)
+		return p1, p2
 # -------------------
 
 # -- snake game helpers --
-def get_mid_cell(snake_game):
-	row_num = len(snake_game.grid)
-	col_num = len(snake_game.grid[0])
-	return snake_game.grid[row_num // 2][col_num // 2].copy()
+def get_mid_cell(snake_grid : list[list[pygame.rect.Rect]]) -> pygame.rect.Rect:
+	row_num = len(snake_grid)
+	col_num = len(snake_grid[0])
+	return snake_grid[row_num // 2][col_num // 2].copy()
 	
 
-def get_rect_surface(snake_game, rect, color):
+def get_rect_surface(snake_game : Snake, rect : pygame.rect.Rect, color : tuple[int, int, int]) -> pygame.Surface:
 	rect_surface = pygame.Surface(rect.size)
 	rect_surface.fill(color)
 	if snake_game.paused: rect_surface.set_alpha(PAUSE_ALPHA)
 	else: rect_surface.set_alpha(NORMAL_ALPHA)
 	return rect_surface
 
-def toggle_pause(snake_game): 
+def toggle_pause(snake_game : Snake) -> None: 
 	if snake_game.snake.alive: snake_game.paused = not snake_game.paused
 	snake_game.snake_GUI.lables['score_lable'].alpha = PAUSE_ALPHA if snake_game.paused else NORMAL_ALPHA
 
-def restart(snake_game):
+def restart(snake_game : Snake) -> None:
 	snake_game.score = 0
 	snake_game.foods = []
 	snake_game.snake_GUI.lables['score_lable'].update_message()
@@ -339,22 +340,22 @@ def restart(snake_game):
 	del snake_game.snake
 	snake_game.snake = SNAKE(snake_game)
 
-def create_grid(snake_game):
+def create_grid(snake_game : Snake) -> None:
 	for h in range(snake_game.surface.get_rect().height // S_CELL_SIZE):
 		row = []
 		for w in range(snake_game.surface.get_rect().width // S_CELL_SIZE):
-			cell = pygame.Rect((w * S_CELL_SIZE, h * S_CELL_SIZE),(S_CELL_SIZE, S_CELL_SIZE))
+			cell = pygame.rect.Rect((w * S_CELL_SIZE, h * S_CELL_SIZE),(S_CELL_SIZE, S_CELL_SIZE))
 			row.append(cell)
 			snake_game.cells.append(cell)
 		snake_game.grid.append(row)
 
-def spawn_food(snake_game, dt):
+def spawn_food(snake_game : Snake, dt : float) -> None:
 	if snake_game.food_timer.dt_wait(dt, FOOD_SPAWN_DELAY):
 		food = choice(snake_game.cells)
 		while not is_valid_food_pos(snake_game, food): food =choice(snake_game.cells)
 		snake_game.foods.append(food)
 
-def is_valid_food_pos(snake_game, food):
+def is_valid_food_pos(snake_game : Snake, food : pygame.rect.Rect) -> bool:
 	return food not in snake_game.snake.body and \
 				food not in snake_game.foods
 # ------------------------
