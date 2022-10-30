8f7acbbe46a59fed4807abc033dd9b7ef8a8d967
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-10-30 18:34:52 +0700

    type hint lable

--- app/GUI/components/lable.py
+++ app/GUI/components/lable.py
@@ -6,11 +6,11 @@ from GUI.components.component import Component
 class Lable(Component):
 	def __init__(self, 
 				 parent, 
-				 message, 
-				 font_size, 
-				 color, 
-				 alpha, 
-				 pos = (0,0), 
+				 message : str, 
+				 font_size : int, 
+				 color : tuple[int, int, int], 
+				 alpha : int, 
+				 pos : tuple[int, int] = (0,0), 
 				 r_topleft = False):
 		super().__init__(parent, pos, (font_size, font_size), alpha, color)
 		self._message = message
@@ -18,11 +18,23 @@ class Lable(Component):
 		self.r_topleft = r_topleft # default is center
 		self.pos = pos
 		self.blink_alpha = alpha
-		self.blink_speed = ALPHA_CHANGE
-		self.blink_change = -1
+		self.blink_speed  : int = ALPHA_CHANGE
+		self.blink_change  : int = -1
 
 		self.center()
-	def blink(self, dt):
+
+	@property
+	def message(self) -> str: return self._message
+
+	@message.setter
+	def message(self, new_message : str) -> None: 
+		self._message = new_message
+		self.update_message()	
+
+	@message.deleter
+	def message(self) -> None: del self._message
+
+	def blink(self, dt : float) -> None:
 		self.alpha +=  (self.blink_speed * dt * self.blink_change)
 
 		if self.alpha <= 0:
@@ -32,21 +44,11 @@ class Lable(Component):
 		if self.alpha >= 255:
 			self.alpha = 255
 			self.blink_change = -1
-	@property
-	def message(self): return self._message
 
-	@message.setter
-	def message(self, new_message): 
-		self._message = new_message
-		self.update_message()	
-
-	@message.deleter
-	def message(self): del self._message
-
-	def center(self):
+	def center(self) -> None:
 		self.surface = self.font.render(self.message, True, self.color)
 		self.rect = self.surface.get_rect(center = self.rect.topleft)
 
-	def update_message(self):
+	def update_message(self) -> None:
 		self.surface = self.font.render(self.message, True, self.color)
 		self.rect = self.surface.get_rect(center = self.rect.center)
